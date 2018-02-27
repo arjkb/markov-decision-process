8754f1f17a0fdfa3d1cdc9fe396de5e527001522
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2018-02-27 14:08:30 -0700

    setting up rewards, possible states etc

--- mdp.py
+++ mdp.py
@@ -2,31 +2,51 @@ import copy
 
 def get_TP(state, action):
     if state == 'TERMINAL_STATE':
-        return (None, 0)
-    elif (state, action) == ('a', 'exit'):
-        return ('TERMINAL_STATE', 1)
-    elif (state, action) == ('b', 'west'):
-        return ('a', 1)
-    elif (state, action) == ('b', 'east'):
-        return ('c', 1)
-    elif (state, action) == ('c', 'west'):
-        return ('b', 1)
-    elif (state, action) == ('c', 'east'):
-        return ('d', 1)
-    elif (state, action) == ('d', 'west'):
-        return ('c', 1)
-    elif (state, action) == ('d', 'east'):
-        return ('e', 1)
-    elif (state, action) == ('e', 'exit'):
-        return ('TERMINAL_STATE', 1)
+        return [(None, 0)]
+    elif (state, action) == ('a', 'go'):
+        [('b', 0.5), ('c', 0.5)]
+    elif (state, action) == ('b', 'go'):
+        [('d', 1)]
+    elif (state, action) == ('c', 'go'):
+        [('a', 0.5), ('e', 0.5)]
+    elif (state, action) == ('d', 'go'):
+        [('f', 1)]
+    elif (state, action) == ('e', 'go'):
+        [('d', 0.5), ('f', 0.5)]
+    elif (state, action) == ('f', 'go'):
+        [('TERMINAL_STATE', 1)]
+
 
 def rewards(state, action, ns):
-    if (state, action, ns) == ('a', 'exit', 'TERMINAL_STATE'):
-        return 20
-    elif (state, action, ns) == ('e', 'exit', 'TERMINAL_STATE'):
-        return 1
-    else:
-        return 0
+    return 1
+
+# def get_TP(state, action):
+#     if state == 'TERMINAL_STATE':
+#         return (None, 0)
+#     elif (state, action) == ('a', 'exit'):
+#         return ('TERMINAL_STATE', 1)
+#     elif (state, action) == ('b', 'west'):
+#         return ('a', 1)
+#     elif (state, action) == ('b', 'east'):
+#         return ('c', 1)
+#     elif (state, action) == ('c', 'west'):
+#         return ('b', 1)
+#     elif (state, action) == ('c', 'east'):
+#         return ('d', 1)
+#     elif (state, action) == ('d', 'west'):
+#         return ('c', 1)
+#     elif (state, action) == ('d', 'east'):
+#         return ('e', 1)
+#     elif (state, action) == ('e', 'exit'):
+#         return ('TERMINAL_STATE', 1)
+
+# def rewards(state, action, ns):
+#     if (state, action, ns) == ('a', 'exit', 'TERMINAL_STATE'):
+#         return 20
+#     elif (state, action, ns) == ('e', 'exit', 'TERMINAL_STATE'):
+#         return 1
+#     else:
+#         return 0
 
 def foo(st, ac):
     if (st, ac) == ('hi', 100):
@@ -35,18 +55,32 @@ def foo(st, ac):
 def main():
     old_values = dict()
     values = dict()
-    discount = 0.2
-    actions = ['east', 'west', 'exit']
-    states = ['TERMINAL_STATE', 'a', 'b', 'c', 'd', 'e']
+    discount = 0.9
+    actions = ['go', 'exit']
+    states = ['TERMINAL_STATE', 'a', 'b', 'c', 'd', 'e', 'f']
 
     possible_actions = {
         'TERMINAL_STATE': [],
-        'a': ['exit'],
-        'b': ['east', 'west'],
-        'c': ['east', 'west'],
-        'd': ['east', 'west'],
-        'e': ['exit']
+        'a': ['go'],
+        'b': ['go'],
+        'c': ['go'],
+        'd': ['go'],
+        'e': ['go'],
+        'f': ['go']
     }
+
+    # discount = 0.2
+    # actions = ['east', 'west', 'exit']
+    # states = ['TERMINAL_STATE', 'a', 'b', 'c', 'd', 'e']
+    #
+    # possible_actions = {
+    #     'TERMINAL_STATE': [],
+    #     'a': ['exit'],
+    #     'b': ['east', 'west'],
+    #     'c': ['east', 'west'],
+    #     'd': ['east', 'west'],
+    #     'e': ['exit']
+    # }
 
     for state in states:
         if state not in old_values:
